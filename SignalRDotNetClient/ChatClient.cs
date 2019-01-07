e9e375a4cf9de7a9e4bd0fd8ee20cae268957fc8
DotNetKolz <dotnetkolz@gmail.com>
2019-01-06 23:41:11 -0500

    Initial check-in of module SignalRDotNetClient

---
+++ SignalRDotNetClient/ChatClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalRDotNetClient
+{
+    public class ChatClient
+    {
+        HubConnection connection;
+        public ChatClient()
+        {
+            connection = new HubConnectionBuilder()
+                                .WithUrl("wss://localhost:5001/chatHub")
+                                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+            connection.On<string, string>(
+                "ReceiveMessage", 
+                (user, message) => 
+                {
+
+                });
+
+        }
+        public async Task SendMessage()
+        {
+            try
+            {
+                await connection.StartAsync();
+                await connection.InvokeAsync(
+                    "SendMessage",
+                    "hey",
+                    "ok");
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
