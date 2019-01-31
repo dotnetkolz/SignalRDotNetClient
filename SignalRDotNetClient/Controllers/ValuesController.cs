f5dde7b3a354236f9b017758b626ffc37f9d2be8
Nikhil <nikhil_koli@cable.comcast.com>
2019-01-31 15:00:47 -0500

    Added groups

--- SignalRDotNetClient/Controllers/ValuesController.cs
+++ SignalRDotNetClient/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRDotNetClient.Controllers
 {
@@ -11,7 +12,6 @@ namespace SignalRDotNetClient.Controllers
     public class ValuesController : ControllerBase
     {
         private readonly ChatClient chat;
-
         public ValuesController()
         {
             chat = new ChatClient();
